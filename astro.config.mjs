1990aed8b79f19bf78e18ea47725b35b09bf66be
Neplod <dorian.ruvel@gmail.com>
2025-11-19 19:21:28 +0100

    feat: Implement initial website structure with new pages, UI components, layouts, and middleware, while updating configurations and removing PostCSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
+
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
 
   markdown: {
     syntaxHighlight: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 
   adapter: vercel(),
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@
 import { defineMiddleware } from 'astro/middleware';
 import { languages } from './i18n/index';
 
-export const onRequest = defineMiddleware(async (context, next) => {
+const onRequest = defineMiddleware(async (context, next) => {
     let lang = 'es';
-    
+
     const cookies = context.cookies.get('divciencia-lang');
     const cookiesTheme = context.cookies.get('divciencia-theme');
 
@@ -15,11 +15,11 @@ export const onRequest = defineMiddleware(async (context, next) => {
         const langFromHeader = acceptLanguage.split(',')[0].split('-')[0];
         lang = langFromHeader;
     }
-    
+
     lang = languages.includes(lang as any) ? lang : 'es';
 
     context.cookies.set('divciencia-lang', lang, { domain: '.divulgandociencia.com', sameSite: 'none', secure: true });
-    
+
     context.locals.lang = lang;
     context.locals.theme = cookiesTheme?.value ?? '';
 
